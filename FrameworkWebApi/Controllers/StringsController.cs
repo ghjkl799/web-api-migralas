7e25944441f87e92ab1bdb03471f36473804f70b
ghjkl799 <ghjkl799@gmail.com>
2022-11-03 11:49:07 +0100

    init

---
+++ FrameworkWebApi/Controllers/StringsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FrameworkWebApi.Controllers
+{
+    public class StringsController : ApiController
+    {
+        // GET api/strings
+        public IEnumerable<string> Get()
+        {
+            return Enumerable.Range(0, 100).Select(q => Guid.NewGuid().ToString());
+        }
+    }
+}
